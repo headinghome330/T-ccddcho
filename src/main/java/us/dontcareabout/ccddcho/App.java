951e5aee2dc8d371dfd35bc4990adacf44ef7b86
MontyPan <montypan@dontcareabout.us>
2021-09-26 18:14:20 +0800

    Init commit.

---
+++ src/main/java/us/dontcareabout/ccddcho/App.java
@@ -0,0 +1,11 @@
+package us.dontcareabout.ccddcho;
+
+public class App {
+	public static void main(String[] args) {
+		test();
+	}
+	
+	public static void test() {
+		System.out.println("WTF world");
+	}
+}
