392752c388fe0e55615e82340645c6a16f68d156
MontyPan <monty.pan@dontcareabout.us>
2021-10-02 11:20:16 +0800

    用 Type 重寫 Card

--- src/main/java/us/dontcareabout/ccddcho/App.java
+++ src/main/java/us/dontcareabout/ccddcho/App.java
@@ -2,19 +2,6 @@ package us.dontcareabout.ccddcho;
 
 public class App {
 	public static void main(String[] args) {
-		//用 0～51 來表示一副撲克牌
-		//cc 版本：大鬼牌 52、小鬼牌 53
-		//ps 版本：大鬼牌 -1、小鬼牌 -2
 
-		for (int i = 0; i < 4; i++) {
-			for (int i2 = 0; i2 < 13; i2++) {
-				Card WTF = new Card(i * 13 + i2);
-				System.out.print(WTF.牌() + '\t');
-			}
-			System.out.println();
-		}
-
-		System.out.println(new Card(-1).牌());
-		System.out.println(new Card(-2).牌());
 	}
 }
--- src/main/java/us/dontcareabout/ccddcho/Card.java
+++ src/main/java/us/dontcareabout/ccddcho/Card.java
@@ -1,47 +1,11 @@
 package us.dontcareabout.ccddcho;
 
 public class Card {
-	private int type;
-	private int value;
+	public final Type type;
+	public final int value;
 
-	/**
-	 * 為了不破壞 interface 而硬留下來的 QQ
-	 * @param number 值域：-2～51，負數代表鬼牌
-	 */
-	public Card(int number) {
-		this(
-			number < 0 ? -1 : number / 13,
-			number < 0 ? Math.abs(number) : number % 13 + 1
-		);
-	}
-
-	/**
-	 * @param type 花色數值，邏輯參見 {@link #花色()}
-	 * @param value 點數
-	 */
-	public Card(int type, int value) {
+	public Card(Type type, int value) {
 		this.type = type;
 		this.value = value;
-	}
-
-	public String 花色() {
-		switch(type) {
-		case -1: return "鬼牌";
-		case 0: return "黑桃";
-		case 1: return "紅心";
-		case 2: return "方塊";
-		case 3: return "梅花";
-		}
-
-		//基本上不會到這邊
-		return "WTF?";
-	}
-
-	public int 點數() {
-		return value;
-	}
-
-	public String 牌() {
-		return 花色() + 點數();
 	}
 }
