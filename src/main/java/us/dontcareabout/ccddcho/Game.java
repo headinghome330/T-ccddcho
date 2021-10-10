519a3347e2f39a57b317badfb245baa0d34539a1
MontyPan <monty.pan@dontcareabout.us>
2021-10-10 10:03:44 +0800

    改用 Deck

--- src/main/java/us/dontcareabout/ccddcho/Game.java
+++ src/main/java/us/dontcareabout/ccddcho/Game.java
@@ -1,8 +1,5 @@
 package us.dontcareabout.ccddcho;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import us.dontcareabout.ccddcho.util.CardUtil;
 
 public class Game {
@@ -10,14 +7,14 @@ public class Game {
 
 	PlayerInfo[] playerInfo;
 
-	List<Card> 暗牌庫;
-	List<Card> 明牌庫 = new ArrayList<>();
+	Deck 暗牌庫 = new Deck();
+	Deck 明牌庫 = new Deck();
 
 	/**
 	 * n 個人各發 24 / n 張牌，然後翻出四張牌進明牌庫
 	 */
 	public Game(int n) {
-		暗牌庫 = CardUtil.genDeck();	//先不洗牌了幹
+		暗牌庫.receive(CardUtil.genDeck());	//先不洗牌了幹
 
 		playerInfo = new PlayerInfo[n];
 
@@ -26,27 +23,25 @@ public class Game {
 
 			//發牌
 			for (int i2 = 0; i2 < 24 / n; i2++) {
-				playerInfo[i].handReceive(暗牌庫.remove(0));
+				playerInfo[i].handReceive(暗牌庫.deal());
 			}
 		}
 
 		//四張明牌
-		for (int i = 0; i < 4; i++) {
-			明牌庫.add(暗牌庫.remove(0));
-		}
+		明牌庫.receive(暗牌庫.deal(4));
 	}
 
 	//偉大的案主口中的「遊戲怎麼進行」
 	public void start() {
 		//TODO
 	}
-	
+
 	private class PlayerInfo {
-		List<Card> hand = new ArrayList<>();
-		List<Card> score = new ArrayList<>();
+		Deck hand = new Deck();
+		Deck score = new Deck();
 
 		public void handReceive(Card card) {
-			hand.add(card);
+			hand.receive(card);
 		}
 	}
 }
