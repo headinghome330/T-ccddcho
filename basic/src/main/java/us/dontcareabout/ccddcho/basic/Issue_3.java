319a4a9ccac6c24c9121d6e2c67d7a5b69150175
headinghome330 <ccddcho@gmail.com>
2021-12-27 01:11:51 +0800

    新增：Issue_3

--- basic/src/main/java/us/dontcareabout/ccddcho/basic/Issue_3.java
+++ basic/src/main/java/us/dontcareabout/ccddcho/basic/Issue_3.java
@@ -1,32 +1,26 @@
 package us.dontcareabout.ccddcho.basic;
 
 public class Issue_3 {
-	/**
-	 * <p>
-	 * Armstrong 數：一個三位數的正整數，假設百位數為 a、十位數為 b、個位數為 c，則滿足 a^3 + b^3 + c^3 = a*100 +
-	 * b*10 + c 為 Armstrong 數。
-	 * 
-	 * @param a 個位數字
-	 * @param b 十位數字
-	 * @param a 百位數字
-	 * 
-	 */
-	public static void armstrong() {
 
-		for (int c = 0; c <= 9; c++) // c loop
-			for (int b = 0; b <= 9; b++) // b loop
-				for (int a = 1; a <= 9; a++) { // a loop
+	public static void main(String[] args) {
+		// Armstrong 數：一個三位數的正整數，假設百位數為 a、十位數為 b、個位數為 c，則滿足 a^3 + b^3 + c^3 = a*100 + b*10 + c 為 Armstrong 數。
+		int a = 1, b = 0, c = 0;
+
+		for (c = 0; c <= 9; c++) {
+			for (b = 0; b <= 9; b++) {
+				for (a = 1; a <= 9; a++) {
+					int k = a * a * a + b * b * b + c * c * c; // >=100 & <1000
 					int m = a * 100 + b * 10 + c;
-					double k = Math.pow(a, 3) + Math.pow(b, 3) + Math.pow(c, 3); // >=100 & <1000
-					if (k == m)
+					if (k == m) {
 						System.out.println(m);
-					else
+					} else
 						continue;
 				}
 
+			}
+
+		}
+
 	}
 
-	public static void main(String[] args) {
-		armstrong();
-	}
 }
