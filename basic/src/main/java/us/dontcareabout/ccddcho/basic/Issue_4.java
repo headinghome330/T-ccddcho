c44cd04869e21a268884493e4252db457158f002
headinghome330 <ccddcho@gmail.com>
2021-12-28 16:18:00 +0800

    將變數 i,j 寫進 for loop

--- basic/src/main/java/us/dontcareabout/ccddcho/basic/Issue_4.java
+++ basic/src/main/java/us/dontcareabout/ccddcho/basic/Issue_4.java
@@ -4,20 +4,18 @@ public class Issue_4 {
 
 	public static void main(String[] args) {
 		int n = 5;
-		int i = 0;
-		int j = 0;
 
 		System.out.println("for loop 金字塔");
 
-		for (i = 0; i < n; i++) {
-			for (j = 0; j <= i; j++) {
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j <= i; j++) {
 				System.out.print("*");
 			}
 			System.out.println();
 		}
 
-		for (i = n - 2; i >= 0; i--) {
-			for (j = 0; j <= i; j++) {
+		for (int i = n - 2; i >= 0; i--) {
+			for (int j = 0; j <= i; j++) {
 				System.out.print("*");
 			}
 			System.out.println();
