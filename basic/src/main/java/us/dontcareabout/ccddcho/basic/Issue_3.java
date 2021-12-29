5557b8d82ab6a852250346cd924004d8c2c1dc69
headinghome330 <ccddcho@gmail.com>
2021-12-29 18:22:34 +0800

    修改：armstrong() 為 caculateArmstrong()

--- basic/src/main/java/us/dontcareabout/ccddcho/basic/Issue_3.java
+++ basic/src/main/java/us/dontcareabout/ccddcho/basic/Issue_3.java
@@ -1,6 +1,5 @@
 package us.dontcareabout.ccddcho.basic;
 
-//aaa+bbb+ccc
 public class Issue_3 {
 	/**
 	 * @param a 百位數字
@@ -8,7 +7,7 @@ public class Issue_3 {
 	 * @param c 個位數字
 	 * @return 傳回值為 a 的三次方 + b 的三次方 + c 的三次方
 	 */
-	public static double armstrong(double a, double b, double c) {
+	public static double caculateArmstrong(double a, double b, double c) {
 		return Math.pow(a, 3) + Math.pow(b, 3) + Math.pow(c, 3);
 	}
 
@@ -23,13 +22,14 @@ public class Issue_3 {
 	}
 
 	public static void main(String[] args) {
-		// Armstrong 數：一個三位數的正整數，假設百位數為 a、十位數為 b、個位數為 c，則滿足 a^3 + b^3 + c^3 = a*100 +
-		// b*10 + c 為 Armstrong 數。
+		// Armstrong 數：一個三位數的正整數，假設百位數為 a、十位數為 b、個位數為 c，則滿足 a^3 + b^3 + c^3 = a*100 + b*10 + c 為 Armstrong 數。
+		
 		System.out.println("method 1");
+		
 		for (int c = 0; c <= 9; c++) {
 			for (int b = 0; b <= 9; b++) {
 				for (int a = 1; a <= 9; a++) {
-//					double k = armstrong(a, b, c);
+//					double k = caculateArmstrong(a, b, c);
 					double k = foo(a) + foo(b) + foo(c);
 					int m = a * 100 + b * 10 + c;
 					if (k == m) {
@@ -44,7 +44,8 @@ public class Issue_3 {
 		for (int m = 100; m < 1000; m++) {
 			int hundred = m / 100;
 			int ten = m % 100 / 10;
-			int each = m % 100 % 10 / 1;
+			int each = m % 100 % 10;
+//			double k = caculateArmstrong(hundred, ten, each);
 			double k = foo(hundred) + foo(ten) + foo(each);
 			if (k == m) {
 				System.out.println(m);
