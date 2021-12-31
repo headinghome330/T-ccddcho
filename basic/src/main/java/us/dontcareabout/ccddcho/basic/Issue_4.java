af553e5fdd80def3c3bedcbab3088c1299b3ad5a
headinghome330 <ccddcho@gmail.com>
2021-12-31 21:11:01 +0800

    補回 35f068c 砍掉的內容

--- basic/src/main/java/us/dontcareabout/ccddcho/basic/Issue_4.java
+++ basic/src/main/java/us/dontcareabout/ccddcho/basic/Issue_4.java
@@ -5,7 +5,24 @@ public class Issue_4 {
 	public static void main(String[] args) {
 		int n = 5;
 		System.out.println("for loop 金字塔");
+		
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j <= i; j++) {
+				System.out.printf("%s","*");
+				
+			}
+			System.out.printf("%n");
+		}
 
+		for (int i = n - 2; i >= 0; i--) {
+			for (int j = 0; j <= i; j++) {
+				System.out.printf("%s","*");
+			}
+			System.out.printf("%n");
+		}
+
+		System.out.println();
+		
 		for (int i = 1; i <= n && i > 0; i = (i == n) ? --i & --n : ++i) {
 			for (int j = 1; j <= i; j++) {
 				System.out.print("*");
