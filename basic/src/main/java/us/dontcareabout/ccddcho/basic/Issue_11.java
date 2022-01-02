9e31bdb04d898d1fddc4b7743bcc51c4cb80e1ba
headinghome330 <ccddcho@gmail.com>
2022-01-02 20:40:15 +0800

    刪除 static field & 修改headSum() / tailSum()

--- basic/src/main/java/us/dontcareabout/ccddcho/basic/Issue_11.java
+++ basic/src/main/java/us/dontcareabout/ccddcho/basic/Issue_11.java
@@ -4,34 +4,35 @@ package us.dontcareabout.ccddcho.basic;
 //注意：上面是偏數學的寫法，所以 index 起始值是 1。（Java 陣列的起始值是 0）
 
 public class Issue_11 {
-	static int[] A = { 3, 6, 2, 1, 4, 4, 2, 1 };
-	
+
 	public static void main(String[] args) {
+
+		int[] A = { 3, 6, 2, 1, 4, 4, 2, 1 };
 
 		for (int i = 1; i <= A.length; i++) {
 			for (int j = 0; j <= A.length; j++) {
-				if (headSum(i - 1) == tailSum(j)) {
-					System.out.println(tailSum(j));
+				if (headSum(i - 1, A) == tailSum(j, A)) {
+					System.out.println(tailSum(j, A));
 				}
 			}
 		}
 
 	}
 
-	public static int headSum(int x) {
+	public static int headSum(int x, int[] A) {
 		int sum = 0;
 
-		for (int i = 0; i <= x && i >= 0; i++) {
+		for (int i = 0; i <= x; i++) {
 			sum += A[i];
 		}
 
 		return sum;
 	}
 
-	public static int tailSum(int y) {
+	public static int tailSum(int y, int[] A) {
 		int sum = 0;
 
-		for (int i = y; i < A.length && i >= 0; i++) {
+		for (int i = y; i <= A.length - 1; i++) {
 			sum += A[i];
 		}
 
