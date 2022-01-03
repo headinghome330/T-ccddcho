99769be5e6d315f837592b576e66b6221524926b
headinghome330 <ccddcho@gmail.com>
2022-01-03 08:57:30 +0800

    改寫 forLoop

--- basic/src/main/java/us/dontcareabout/ccddcho/basic/Issue_4.java
+++ basic/src/main/java/us/dontcareabout/ccddcho/basic/Issue_4.java
@@ -4,30 +4,45 @@ public class Issue_4 {
 
 	public static void main(String[] args) {
 		int n = 5;
+		int m = 5;
 		System.out.println("for loop 金字塔");
-		
+
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j <= i; j++) {
-				System.out.printf("%s","*");
-				
+				System.out.printf("%s", "*");
+
 			}
 			System.out.printf("%n");
 		}
 
 		for (int i = n - 2; i >= 0; i--) {
 			for (int j = 0; j <= i; j++) {
-				System.out.printf("%s","*");
+				System.out.printf("%s", "*");
 			}
 			System.out.printf("%n");
 		}
 
-		System.out.println();
-		
-		for (int i = 1; i <= n && i > 0; i = (i == n) ? --i & --n : ++i) {
+		System.out.println("for loop 金字塔");
+
+		for (int i = 1; i <= n && i > 0; i = (i == n) ? --n & --i : ++i) {
 			for (int j = 1; j <= i; j++) {
 				System.out.print("*");
 			}
 			System.out.println();
+		}
+
+		System.out.println("for loop 金字塔");
+
+		for (int i = 1; i <= m & m > 0; i = (i == m) ? i : ++i) {
+			for (int j = 1; j <= i; j++) {
+				System.out.print("*");
+
+			}
+			System.out.println();
+			if (i == m) {
+				i--;
+				m--;
+			}
 		}
 
 	}
