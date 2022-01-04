af1c87aa4b7f1a2c2016e1edfbbe68c8f06826db
headinghome330 <ccddcho@gmail.com>
2022-01-04 10:57:15 +0800

    修改 threeCubed() 為 cubed() 及加入負指數算法

--- basic/src/main/java/us/dontcareabout/ccddcho/basic/Issue_3.java
+++ basic/src/main/java/us/dontcareabout/ccddcho/basic/Issue_3.java
@@ -1,7 +1,7 @@
 package us.dontcareabout.ccddcho.basic;
 
 public class Issue_3 {
-	
+
 	/**
 	 * @param a 百位數字
 	 * @param b 十位數字
@@ -14,34 +14,35 @@ public class Issue_3 {
 
 	/**
 	 * @param v 傳入數字
-	 * @param p 次方數
+	 * @param p 次方整數
 	 * @return v^p 數值
 	 */
-	public static int threeCubed(int v, int p) {
-		int threeCubed = 1;
-
-		for (int i = 0; i <= p; i++) { 
-			for (int j = 0; j <= i; j++) {
-				if (j == 0) {
-					threeCubed = 1;
-				} else {
-					threeCubed = v * threeCubed;
-				}
+	public static double cubed(int v, int p) {
+		double Cubed = 1;
+		boolean b;
+		
+		for (int j = 1; b = (p >= 0) ? (j <= p) : (j <= -p); j++) {
+			if (j == 1 && p == 0) {
+				Cubed = 1;
+			} else if (b == (j <= p)) {
+				Cubed *= v;
+			} else if (b == (j >= p)) {
+				Cubed /= v;
 			}
 		}
-		
-		return threeCubed;
+
+		return Cubed;
 	}
-	
+
 	public static void main(String[] args) {
 		// Armstrong 數：一個三位數的正整數，假設百位數為 a、十位數為 b、個位數為 c，則滿足 a^3 + b^3 + c^3 = a*100 + b*10 + c 為 Armstrong 數。
 		
 		System.out.println("method 1");
-		
+
 		for (int c = 0; c <= 9; c++) {
 			for (int b = 0; b <= 9; b++) {
 				for (int a = 1; a <= 9; a++) {
-					double k = threeCubed(a,3) + threeCubed(b,3) + threeCubed(c,3);
+					double k = cubed(a, 3) + cubed(b, 3) + cubed(c, 3);
 					int m = a * 100 + b * 10 + c;
 					if (k == m) {
 						System.out.println(m);
@@ -49,18 +50,18 @@ public class Issue_3 {
 				}
 			}
 		}
-		
+
 		System.out.println("method 2");
-		
+
 		for (int m = 100; m < 1000; m++) {
 			int hundred = m / 100;
 			int ten = m % 100 / 10;
 			int each = m % 100 % 10;
-			double k = threeCubed(hundred,3) + threeCubed(ten,3) + threeCubed(each,3);
+			double k = cubed(hundred, 3) + cubed(ten, 3) + cubed(each, 3);
 			if (k == m) {
 				System.out.println(m);
 			}
 		}
-		
+
 	}
 }
