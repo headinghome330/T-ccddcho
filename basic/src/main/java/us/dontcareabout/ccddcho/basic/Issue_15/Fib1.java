c46b5df089dfe9b5f2d98213cc4ccd58f371fb46
headinghome330 <ccddcho@gmail.com>
2022-01-21 02:05:33 +0800

    修改instance不能夠重覆call的問題及修改 0<n<2 的return錯誤

--- basic/src/main/java/us/dontcareabout/ccddcho/basic/Issue_15/Fib1.java
+++ basic/src/main/java/us/dontcareabout/ccddcho/basic/Issue_15/Fib1.java
@@ -2,23 +2,33 @@ package us.dontcareabout.ccddcho.basic.Issue_15;
 
 public class Fib1 {
 
-	private int s;
-	private int f;
+	private int a0;
+	private int a1;
 
-	public Fib1(int s, int f) {
-		this.s = s;
-		this.f = f;
+	public Fib1(int a0, int a1) {
+		this.a0 = a0;
+		this.a1 = a1;
 	}
 
-	public int fibonacci(int d) {
+	public int fibonacci(int n) {
 		int result = 0;
-		
-		for (int i = 2; i <= d; i++) {
-			result = s + f;
-			s = f;
-			f = result;
+		int first  = a0;
+		int second = a1;
+
+		for (int i = 0; i <= n; i++) {
+			if (i == 0) {
+				result = first;
+			} else if (i == 1) {
+				result = second;
+			} else {
+				result = a0 + a1;
+				a0 = a1;
+				a1 = result;
+			}
 		}
 
+		a0 = first;
+		a1 = second;
 		return result;
 	}
 }
