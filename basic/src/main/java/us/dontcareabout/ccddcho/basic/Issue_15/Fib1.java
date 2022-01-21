0e30974033e6dc1fb6240f2ba6ebc7a119cbea43
headinghome330 <ccddcho@gmail.com>
2022-01-21 17:03:03 +0800

    修改 Fib1()，並使用 guard clause

--- basic/src/main/java/us/dontcareabout/ccddcho/basic/Issue_15/Fib1.java
+++ basic/src/main/java/us/dontcareabout/ccddcho/basic/Issue_15/Fib1.java
@@ -1,9 +1,13 @@
 package us.dontcareabout.ccddcho.basic.Issue_15;
 
+/**
+ * 因為在原本的版本 L30、L31 不需要寫，每次的傳入都會 initial
+ *
+ */
 public class Fib1 {
 
-	private int a0;
-	private int a1;
+	private final int a0;
+	private final int a1;
 
 	public Fib1(int a0, int a1) {
 		this.a0 = a0;
@@ -12,23 +16,22 @@ public class Fib1 {
 
 	public int fibonacci(int n) {
 		int result = 0;
-		int first  = a0;
-		int second = a1;
-
-		for (int i = 0; i <= n; i++) {
-			if (i == 0) {
-				result = first;
-			} else if (i == 1) {
-				result = second;
-			} else {
-				result = a0 + a1;
-				a0 = a1;
-				a1 = result;
+		int v1 = a0;
+		int v2 = a1;
+		
+		
+		if (n == 0) {
+			return v1;
+		} else if (n == 1) {
+			return v2;
+		} else {
+			for (int i = 2; i <= n; i++) {
+				result = v1 + v2;
+				v1 = v2;
+				v2 = result;
 			}
+			return result;
 		}
 
-		a0 = first;
-		a1 = second;
-		return result;
 	}
 }
