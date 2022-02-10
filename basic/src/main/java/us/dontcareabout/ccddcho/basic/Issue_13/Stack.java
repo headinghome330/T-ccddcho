8ad8dad98a6089d5ed1a0e0e66ffbf48a81e8dc0
headinghome330 <ccddcho@gmail.com>
2022-02-10 13:51:30 +0800

    修改 push()、pop()及增加 order()

--- basic/src/main/java/us/dontcareabout/ccddcho/basic/Issue_13/Stack.java
+++ basic/src/main/java/us/dontcareabout/ccddcho/basic/Issue_13/Stack.java
@@ -1,5 +1,7 @@
 package us.dontcareabout.ccddcho.basic.Issue_13;
 
+import java.util.Arrays;
+
 /**
  * 目前的版本修改了原本多一個 index 的部分，當 push() 時同時在兩個 stack 上增加數字，分別是 stack[] 及 temp[]
  * stack[] 依然保有完整的 push() 紀錄，而 temp() 則是隨著 pop() 而去 remove() 內容 保有 stack[]
@@ -10,33 +12,41 @@ package us.dontcareabout.ccddcho.basic.Issue_13;
 public class Stack {
 
 	private int[] result = new int[0];
+	private int topV;
 
 	public int pop() {
-		int topV = result[result.length - 1];
+		int k = 0;
+		k = order(k);
+		topV = result[k];
+		result[k] = 0;
+		return topV;
+	}
 
-		for (int i0 = result.length - 1; i0 > 0; i0--) {
-			result[i0] = result[i0 - 1];
-			result[0] = topV;
+	public int order(int k) {
+		if (result[k] == 0 && k < result.length / 2) {
+			k++;
+			return order(k);
 		}
 
-		return topV;
+		return k;
 	}
 
 	public void push(int k) {
 		result = add(k, result);
 	}
 
-	private static int[] add(int k, int[] a) {
-		int[] lastResult = a;
-		a = new int[a.length + 1];
+	private int[] add(int k, int[] lastResult) {
+
+		int[] a = new int[lastResult.length + 2];
+		a[0] = k;
 		a[a.length - 1] = k;
 
-		if (a.length != 0) {
-			for (int i1 = lastResult.length - 1; i1 >= 0; i1--) {
-				a[i1] = lastResult[i1];
-			}
+		for (int i = 1; i < a.length - 1; i++) {
+			a[i] = lastResult[i - 1];
 		}
 
 		return a;
+
 	}
+
 }
